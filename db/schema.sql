1a8a3222fd49ebac4f2cd00f3d583ded3ab75ff1
liaof <f.liao163@gmail.com>
2021-05-07 01:48:22 -0400

    12.3.4

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,11 @@
 /* to use, type 'source db/schema.sql' in the sql cmd line*/
+
+CREATE TABLE parties (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
+);
+
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,/*PRIMARY KEY indicates this value will be our index, and that it is an INGTEGER that automatically increments*/
   first_name VARCHAR(30) NOT NULL,/* column called first_name that is a string of 30 or less chars, whose value cannot be null*/
